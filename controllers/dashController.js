ad8cd1702e80ee8ff5b93a20365c83ad9edaf2c8
Git-Vdim-Hub <vadimrace@gmail.com>
2023-02-18 09:19:58 -0500

    working dash display for user 1

--- controllers/dashController.js
+++ controllers/dashController.js
@@ -1,3 +1,20 @@
-exports.get = (req, res) =>{
-    res.render('dash');
+const {User, Post} = require('../models');
+
+exports.get = async (req, res) =>{
+    try{
+        const postObjects = await Post.findAll({
+            include: [
+                {model: User}
+            ],
+            where: {user_id: 1}
+            //where: { user_id: req.session.user_id }
+        })
+        if(postObjects.length > 0) {
+            const dataForPosts = postObjects.map((data) =>
+            data.get({plain: true}));
+            res.render('dash');
+        }
+    } catch (err) {
+
+    }
 }
