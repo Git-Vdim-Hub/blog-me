2fba7759391f96443aee9cc1486849162704f43e
Git-Vdim-Hub <vadimrace@gmail.com>
2023-02-19 17:09:12 -0500

    user login/session fully routed

---
+++ public/js/logIn.js
@@ -0,0 +1,25 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+    userInput = document.querySelector('#user-input').value;
+    console.log(userInput);
+    passInput = document.querySelector('#password-input').value;
+    console.log(passInput);
+    if(userInput && passInput){
+        try{
+            const response = await fetch('/api/users/login', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({userInput, passInput})
+            });
+            if(response.ok){
+                document.location.replace('/dashboard');
+            }
+        }catch (err) {
+            console.log(err);
+        }
+    } else{
+        alert("You cannot leave fields blank");
+    }
+}
+
+document.querySelector('#login-button').addEventListener('click', loginFormHandler);
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -18,6 +18,34 @@ router.post('/signup', async (req, res) => {
 } catch (err) {
     res.status(500).json(err);
 }
+});
+//localhost:3000/api/users/login
+router.post('/login', async (req, res) => {
+  try{
+    const userData = await User.findOne({
+      where: {
+        username: req.body.userInput
+      }
+    });
+    if(!userData) {
+      res.status(400).json({message: 'Incorrect email or password. Please try again.' });
+      return;
+    }
+    const validPassword = await userData.checkPassword(req.body.passInput);
+    console.log("VALID OR INVALID", validPassword);
+    if(!validPassword){
+      res.status(400).json({message: 'Incorrect email or password. PLease try again!'});
+      return;
+    }
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json({ user: userData, message: 'You are now logged in!'});
+    })
+  } catch (err){
+    console.log(err);
+    res.status(500).json(err);
+  }
 })
 
 module.exports = router;
